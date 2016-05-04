1c9f94be7cb77c3f15e9beae2d099a0444e796de
Nick Dyer <nbdyer@gmail.com>
2016-05-04 15:46:33 +0100

    Tests passing for second assignment, but very slow

--- src/test/java/uk/nickbdyer/datastructures/treeHeightTest.java
+++ src/test/java/uk/nickbdyer/datastructures/treeHeightTest.java
@@ -33,4 +33,22 @@ public class treeHeightTest {
         assertEquals("4\n", outContent.toString());
     }
 
+    @Test
+    public void computes100NodeTree() throws IOException {
+        String input = "100\n" +
+                "98 30 92 96 15 78 40 20 24 44 90 82 61 15 28 53 51 19 57 4 37 70 38 21 79 19 48 65 25 76 21 75 45 15 82 64 72 85 65 21 77 55 45 65 29 50 66 74 10 41 73 25 15 18 84 30 50 7 44 36 48 36 52 16 73 97 80 74 48 22 74 9 88 69 89 7 49 7 12 9 95 1 17 80 34 44 99 79 4 -1 61 57 1 81 5 58 36 37 40 40";
+        inContent = new ByteArrayInputStream(input.getBytes());
+        tree_height tree = new tree_height();
+        tree.run(new InputStreamReader(inContent), new PrintStream(outContent));
+        assertEquals("28\n", outContent.toString());
+    }
+
+    @Test
+    public void computes100000NodeTree() throws IOException {
+        tree_height tree = new tree_height();
+        tree.run(new FileReader("src/test/java/uk/nickbdyer/datastructures/100000Nodes"), new PrintStream(outContent));
+        assertEquals("50001\n", outContent.toString());
+    }
+
+
 }
