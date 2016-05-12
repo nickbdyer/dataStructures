203850e34ac4177dcaff613fe9d31c27ac01b937
Nick Dyer <nbdyer@gmail.com>
2016-05-12 15:36:46 +0100

    Adds basic starter file for phoneBook assignment, needs configuring

---
+++ src/test/java/uk/nickbdyer/datastructures/PhoneBookTest.java
@@ -0,0 +1,12 @@
+package uk.nickbdyer.datastructures;
+
+import org.junit.Test;
+
+public class PhoneBookTest {
+    
+    @Test
+    public void sampleTestOne() {
+        PhoneBook phoneBook = new PhoneBook();
+
+    }
+}
