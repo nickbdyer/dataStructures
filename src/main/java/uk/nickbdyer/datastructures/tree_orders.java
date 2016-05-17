99035a2680e9578eec55bfcf01863411fe4fedf1
Nick Dyer <nbdyer@gmail.com>
2016-05-17 15:55:03 +0100

    Adds starter file, adjusted for testing for binary tree traversal asssignment

--- src/main/java/uk/nickbdyer/datastructures/tree_orders.java
+++ src/main/java/uk/nickbdyer/datastructures/tree_orders.java
@@ -1,4 +1,94 @@
 package uk.nickbdyer.datastructures;
 
+import java.util.*;
+import java.io.*;
+
 public class tree_orders {
+    class FastScanner {
+        StringTokenizer tok = new StringTokenizer("");
+        BufferedReader in;
+
+        FastScanner(InputStreamReader input) {
+            in = new BufferedReader(input);
+        }
+
+        String next() throws IOException {
+            while (!tok.hasMoreElements())
+                tok = new StringTokenizer(in.readLine());
+            return tok.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public class TreeOrders {
+        int n;
+        int[] key, left, right;
+
+        void read(InputStreamReader input) throws IOException {
+            FastScanner in = new FastScanner(input);
+            n = in.nextInt();
+            key = new int[n];
+            left = new int[n];
+            right = new int[n];
+            for (int i = 0; i < n; i++) {
+                key[i] = in.nextInt();
+                left[i] = in.nextInt();
+                right[i] = in.nextInt();
+            }
+        }
+
+        List<Integer> inOrder() {
+            ArrayList<Integer> result = new ArrayList<Integer>();
+            // Finish the implementation
+            // You may need to add a new recursive method to do that
+
+            return result;
+        }
+
+        List<Integer> preOrder() {
+            ArrayList<Integer> result = new ArrayList<Integer>();
+            // Finish the implementation
+            // You may need to add a new recursive method to do that
+
+            return result;
+        }
+
+        List<Integer> postOrder() {
+            ArrayList<Integer> result = new ArrayList<Integer>();
+            // Finish the implementation
+            // You may need to add a new recursive method to do that
+
+            return result;
+        }
+    }
+
+    static public void main(String[] args) throws IOException {
+        new Thread(null, new Runnable() {
+            public void run() {
+                try {
+                    new tree_orders().run(new InputStreamReader(System.in), new PrintStream(System.out));
+                } catch (IOException e) {
+                }
+            }
+        }, "1", 1 << 26).start();
+    }
+
+    public void print(List<Integer> x, PrintStream output) {
+        for (Integer a : x) {
+            output.print(a + " ");
+        }
+        output.println();
+    }
+
+    public void run(InputStreamReader input, PrintStream output) throws IOException {
+        TreeOrders tree = new TreeOrders();
+        tree.read(input);
+        print(tree.inOrder(), output);
+        print(tree.preOrder(), output);
+        print(tree.postOrder(), output);
+    }
 }
+
--- src/test/java/uk/nickbdyer/datastructures/tree_orders_test.java
+++ src/test/java/uk/nickbdyer/datastructures/tree_orders_test.java
@@ -1,4 +1,57 @@
 package uk.nickbdyer.datastructures;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.junit.Assert.assertEquals;
+
 public class tree_orders_test {
+
+    private ByteArrayOutputStream outContent;
+    private ByteArrayInputStream inContent;
+
+    @Before
+    public void setUp() {
+        outContent = new ByteArrayOutputStream();
+    }
+
+    @Test
+    public void traverses5NodeTree() throws IOException {
+        String input = "5\n" +
+                       "4 1 2\n" +
+                       "2 3 4\n" +
+                       "5 -1 -1\n" +
+                       "1 -1 -1\n" +
+                       "3 -1 -1\n";
+        inContent = new ByteArrayInputStream(input.getBytes());
+        tree_orders orders = new tree_orders();
+        orders.run(new InputStreamReader(inContent), new PrintStream(outContent));
+        assertEquals("1 2 3 4 5\n" +
+                     "4 2 1 3 5\n" +
+                     "1 3 2 4 5\n", outContent.toString());
+
+    }
+    
+    @Test
+    public void traverses10NodeTree() throws IOException {
+        String input = "10\n" +
+                       "0 7 2\n" +
+                       "10 -1 -1\n" +
+                       "20 -1 6\n" +
+                       "30 8 9\n" +
+                       "40 3 -1\n" +
+                       "50 -1 -1\n" +
+                       "60 1 -1\n" +
+                       "70 5 4\n" +
+                       "80 -1 -1\n" +
+                       "90 -1 -1\n";
+        inContent = new ByteArrayInputStream(input.getBytes());
+        tree_orders orders = new tree_orders();
+        orders.run(new InputStreamReader(inContent), new PrintStream(outContent));
+        assertEquals("50 70 80 30 90 40 0 20 10 60\n" +
+                     "0 70 50 40 30 80 90 20 60 10\n" +
+                     "50 80 90 30 40 70 10 60 20 0\n", outContent.toString());
+    }
 }
